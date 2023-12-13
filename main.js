c1124ea2e0519bb8e0ad06f7e48566b0fd3e8202
nwflfawwaz <149747613+fawwaznwfl@users.noreply.github.com>
2023-12-13 10:42:57 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const menuIcon = document.getElementById("menu-icon");
+const menulist = document.getElementById("menu-list");
+
+menuIcon.addEventListener("click", () => {
+  menulist.classList.toggle("hidden");
+});
